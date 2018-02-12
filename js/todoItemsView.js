9407bda0e510b16189ba1e6e6b347a19207f0294
sleeplesseditor <josh@bryson.me.uk>
2018-02-12 16:16:34 +0000

    added ability to add new TodoItems

--- js/todoItemsView.js
+++ js/todoItemsView.js
@@ -6,10 +6,43 @@ var TodoItemsView = Backbone.View.extend({
     initialize: function(options){
         if (!(options && options.model))
             throw new Error("model is not specified.");
+    
+        this.model.on("add", this.onAddTodoItem, this);
+    },
+
+    //Handles Collection Events
+    onAddTodoItem: function(todoItem){
+        var view = new TodoItemView({ model: todoItem });
+        this.$el.append(view.render().$el);
+    },
+
+    //Handles DOM Events
+    events: {
+        "click #add": "onClickAdd",
+        "keypress #newTodoItem": "onKeyPress"
+    },
+
+    onKeyPress: function(e){
+        if (e.keyCode == 13)
+            this.onClickAdd();
+    },
+
+    onClickAdd: function(){
+        var $textBox = this.$("#newTodoItem");
+
+        if ($textBox.val()){
+            var todoItem = new TodoItem({ description: $textBox.val() });
+            this.model.add(todoItem);
+
+            $textBox.val("");
+        }
     },
 
     render: function(){
         var self = this;
+
+        this.$el.append("<input type='text' autofocus id='newTodoItem'></input>");
+        this.$el.append("<button id='add'>Add</button>");
 
         this.model.each(function(todoItem){
             var view = new TodoItemView({ model: todoItem });
