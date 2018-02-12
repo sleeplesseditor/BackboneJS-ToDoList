7c66926ef2c4cf101de6bb93c7fd9d076a75cb2a
sleeplesseditor <josh@bryson.me.uk>
2018-02-12 18:27:22 +0000

    added item toggle feature

--- js/todoItem.js
+++ js/todoItem.js
@@ -1,7 +1,17 @@
 var TodoItem = Backbone.Model.extend({
+    defaults: {
+        isCompleted: false
+    },
+
+    //Validation of model
     validate: function(attrs){
         if (!attrs.description)
             return "Description is required.";
+    },
+
+    //Toggle for updating state of model
+    toggle: function(){
+        this.set("isCompleted", !this.get("isCompleted"));
     }
 });
 
