2e39393dd439f007deb9b8034ee322ce966f54bc
sleeplesseditor <josh@bryson.me.uk>
2018-02-12 23:38:48 +0000

    Mustache.js templating added

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Backbone.js ToDoList
-This is a simple ToDoList app to demonstrate the use of Backbone.js.
+This is a simple ToDoList app to demonstrate the use of Backbone.js, as well as the use of mustache.js as a templating engine.
 
 The app uses the JSONPlaceholder API to provide fake todos.
--- js/todoItemView.js
+++ js/todoItemView.js
@@ -27,8 +27,9 @@ var TodoItemView = Backbone.View.extend({
 
         this.$el.toggleClass("completed", this.model.get("completed"));
 
-        var checked = this.model.get("completed") ? "checked" : "";
-        this.$el.html("<input id='toggle' type='checkbox'" + checked + "></input>" + this.model.escape("title") + "<button id='delete'>Delete</button>");
+        var template = $("#todoItemTemplate").html();
+        var html = Mustache.render(template, this.model.toJSON());
+        this.$el.html(html);
 
         return this;
     }
